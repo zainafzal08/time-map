e36ba3ba47f7072b871e63132a544e25cd66b7a0
Zain Afzal <zain.afz@gmail.com>
2018-06-27 12:27:09 +1000

    lol

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="card group">
     <div class="header">
-      <h1 class="title"> {{group.name}} </h1>
+      <h1 class="card-title card-title-left"> {{group.name}} </h1>
       <hr>
-      <div class="group-stats">
-        <div><i class="mdi mdi-clock"></i>{{group.stats.upcomingEvents}}</div>
-        <div><i class="mdi mdi-chart-line"></i>{{group.stats.totalEvents}}</div>
-        <div><i class="mdi mdi-account"></i>{{group.stats.members}}</div>
+      <br>
+      <div class="members">
+        <img class="member" src="https://www.ienglishstatus.com/wp-content/uploads/2018/04/Cute-Whatsapp-DP.jpg"></img>
+        <img class="member" src="https://www.ienglishstatus.com/wp-content/uploads/2018/04/Anonymous-Whatsapp-profile-picture.jpg"></img>
       </div>
     </div>
-    <div v-if="group.members.length < 5" class="content">
-      <img v-for="m in group.members" class="member" :src="m.imgSrc"></img>
-    </div>
-    <div v-else class="content">
-      <img v-for="m in group.members.slice(0,4)" class="member" :src="m.imgSrc"></img>
+    <div class="content">
+
     </div>
     <div class="footer">
       <div class="btns">
@@ -41,12 +38,32 @@ export default {
   height: 200px;
   margin-right: 2rem;
 }
-
+.group i{
+  margin-left: 1rem;
+  color: #BBB;
+  font-size: 1.5rem;
+}
+.group .member {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-left:-15px;
+}
+.group .members {
+  margin-left: calc(10% + 15px);
+}
 .group .header {
   width: 100%;
   height: 50%;
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
+  text-align: left;
+}
+.group .card-title {
+  font-size: 1.1rem;
+  color: #777;
+  font-weight:200;
+  margin-left: 10%;
 }
 .group .group-stats {
   display: flex;
@@ -59,7 +76,7 @@ export default {
   margin-left: 10%;
   margin-top: 1rem;
 }
-.group .group-stats i {
+.group i {
   margin-right: 0.5rem;
 }
 .group .content {
@@ -68,21 +85,6 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: rgb(245,245,245);
-}
-.group .content i{
-  margin-left: 1rem;
-  color: #BBB;
-  font-size: 1.5rem;
-}
-.group .content .member {
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  opacity: 0.7;
-  margin-left: 0.25rem;
-  margin-right: 0.25rem;
-  box-shadow: 0 1px 4px rgba(0,0,0,0.16), 0 1px 4px rgba(0,0,0,0.23);
 }
 .group .footer {
   width: 100%;
@@ -91,6 +93,7 @@ export default {
   border-bottom-right-radius: 15px;
   display: flex;
   align-items: center;
+  background-color: rgb(245,245,245);
 }
 
 .group .footer .btns {
